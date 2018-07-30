52f2c403065fe27eabbcf172be0107a10ffb2329
tao.xu <tao.xu@qunar.com>
2018-07-30 09:49:22 +0800

    add 注释链接

--- webpack4-demo/webpack-tree/src/index.js
+++ webpack4-demo/webpack-tree/src/index.js
@@ -7,5 +7,5 @@ class App extends PureComponent {
 		return <div>{ minus(100,20)}</div>;
 	}
 }
-
+//http://webpack.wuhaolin.cn/4%E4%BC%98%E5%8C%96/4-10%E4%BD%BF%E7%94%A8TreeShaking.html
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack4-demo/webpack-tree/src/util.js
+++ webpack4-demo/webpack-tree/src/util.js
@@ -1,7 +1,10 @@
 export function add(a, b) {
+	console.log('adding');
 	return a + b;
 }
 
 export function minus(a, b) {
+	console.log('minus');
+	
 	return a - b;
 }
